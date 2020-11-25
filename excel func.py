a3a3dbfff43d53439db8b53a670c9f8a450683f9
Tasyke <71439117+Tasyke@users.noreply.github.com>
2020-11-25 12:24:27 +0400

    fixed

--- excel func.py
+++ excel func.py
@@ -1,10 +1,8 @@
 import xlwt 
 from xlwt import Workbook 
   
-# Workbook is created 
 wb = Workbook() 
   
-# add_sheet is used to create sheet. 
 sheet1 = wb.add_sheet('Sheet 1') 
   
 sheet1.write(1, 0, 'орТём лох') 
